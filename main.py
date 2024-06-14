d7ed19b53bac6cf822a8a2c998bba427b760f258
karin.a.kazarian <karin.b.kazarian@ericsson.com>
2024-06-14 13:59:51 -0400

    fix the port

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ async def predict(request: Request, input_data: str = Form(...), steps: str = Fo
 #     return predictions
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1')
